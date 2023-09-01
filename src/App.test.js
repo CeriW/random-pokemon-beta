41add49be5418a7b81694721a4dcb13d07920102
Ceri <ceriwoolway@gmail.com>
2023-09-01 18:25:28 +0100

    Pages updates

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './index';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// test('1 + 2 equals 3', () => {
+//   const result = 1 + 2;
+//   expect(result).toBe(3);
+// });
+
+// test('a condition is false', () => {
+//   const condition = false;
+//   expect(condition).toBeFalsy();
+// });
